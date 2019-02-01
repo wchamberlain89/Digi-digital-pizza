b3e31640ed22f1fa15edd51027d1f4a7eb0f72c8
wchamberlain89 <wchamberlain89@gmail.com>
2019-02-01 10:30:43 -0800

    it will create a pizza object with toppings and calculate price based on toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-function Pizza(size) {
+function Pizza(size, meats, veggies) {
 	this.size = size;
-	// this.toppings = toppingsArray;
+	this.meats = meats;
+	this.veggies = veggies;
 	this.price = this.getPrice();
 }
 
 Pizza.prototype.getPrice = function() {
-	console.log(this.size);
 	if (this.size === 'sm') {
-		return 10;
+		return 10.00 + (this.meats.length * 1.25) + (this.veggies.length * 1.00);
 	} else if (this.size === 'md') {
-		return 14;
+		return 14 + (this.meats.length * 1.50) + (this.veggies.length * 1.25);
 	} else {
-		return 16;
+		return 16 + (this.meats.length * 1.75) + (this.veggies.length * 1.50);
 	}
 }
 
@@ -21,9 +21,19 @@ function Topping(name, price) {
 }
 
 function getMeatToppings () {
+	var toppings = [];
 	$("#meat-select input:checkbox[name='meat-topping']:checked").each(function(topping){
-		console.log($(this).val());
+		toppings.push($(this).val());
 	});
+	return toppings;
+}
+
+function getVeggieToppings () {
+	var toppings = [];
+	$("#veggie-select input:checkbox[name='veggie-topping']:checked").each(function(topping){
+		toppings.push($(this).val());
+	});
+	return toppings;
 }
 
 
@@ -35,7 +45,8 @@ $("#pizza-form").submit(function(event){
 		event.preventDefault();
 		var sizeInput = $("input[name='sizes']:checked").val();
 		var meatToppings = getMeatToppings();
-		var userPizza = new Pizza(sizeInput);
+		var veggieToppings = getVeggieToppings();
+		var userPizza = new Pizza(sizeInput, meatToppings, veggieToppings);
 		console.log(userPizza.price); 
 	});
 });
