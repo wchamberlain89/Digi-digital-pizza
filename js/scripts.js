409bca6648f716e2e07a2510fb2990bd2c1828f1
wchamberlain89 <wchamberlain89@gmail.com>
2019-02-01 10:19:22 -0800

    it will take meat checkboxes and log each value to console

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,12 @@ function Topping(name, price) {
 	this.price = price;
 }
 
+function getMeatToppings () {
+	$("#meat-select input:checkbox[name='meat-topping']:checked").each(function(topping){
+		console.log($(this).val());
+	});
+}
+
 
 
 
@@ -28,6 +34,7 @@ $(function(){
 $("#pizza-form").submit(function(event){
 		event.preventDefault();
 		var sizeInput = $("input[name='sizes']:checked").val();
+		var meatToppings = getMeatToppings();
 		var userPizza = new Pizza(sizeInput);
 		console.log(userPizza.price); 
 	});
